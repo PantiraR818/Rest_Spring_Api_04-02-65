61b56cb26dfe9971c5b01e7fb0810c0b65c64b36
PantiraR818 <PantiraPloy818@gmail.com>
2022-02-04 17:28:40 +0700

    first commit

---
+++ src/main/java/nvc/it/springapi/model/Product.java
@@ -0,0 +1,35 @@
+package nvc.it.springapi.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// Json
+@Document("products")
+@Getter @Setter @NoArgsConstructor
+public class Product {
+    @Id
+    private String _id;
+    private String name;
+    private Double price;
+    private Integer unit_in_stock;
+
+    // เก็บตัวรีวิว Review
+    private List<Review> reviews = new ArrayList<Review>();
+    
+    // คงที่
+    @CreatedDate
+    private Date createdAt;
+    // เมื่อมีการอัพเดตจะตรงนี้ที่อัพเดตตรงเดียว
+    @LastModifiedBy
+    private Date updatedAt;
+}
